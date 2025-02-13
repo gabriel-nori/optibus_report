52db91434e7797ee7469d94d67980fb010e4fb27
gabriel.nori <gabrielnori@gabrielnori.tech>
2025-02-13 02:53:07 -0300

    Fix: Fixes a bug when comparing env variable to use relative path

--- app/config/settings.py
+++ app/config/settings.py
@@ -7,7 +7,7 @@ env_file_path = os.getcwd() + "/.env"
 env_loaded = load_dotenv(dotenv_path=env_file_path, override=True)
 
 LOG_LEVEL = os.getenv("LOG_LEVEL", "info")
-USE_RELATIVE_PATH = os.getenv("USE_RELATIVE_PATH", "True") == True
+USE_RELATIVE_PATH = os.getenv("USE_RELATIVE_PATH", "True") == "True"
 FILE_INGESTION_PATH = os.getenv("FILE_INGESTION_PATH", "/ingestion/")
 FILE_OUTPUT_PATH = os.getenv("FILE_OUTPUT_PATH", "/output/")
 
